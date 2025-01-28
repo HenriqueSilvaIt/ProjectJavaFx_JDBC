55c0f68b754118680df3396da7020863f35e10a3
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-28 08:34:48 -0300

    Main view Designer

--- src/main/java/com/dev/projectjavafxjdbc/HelloApplication.java
+++ src/main/java/com/dev/projectjavafxjdbc/HelloApplication.java
@@ -2,19 +2,38 @@ package com.dev.projectjavafxjdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            // Instanciando programa passando o pacote da View:
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+            // Carregando a view
+            ScrollPane scrollPane = loader.load(); // Trocar de parent para ScrollPane
+
+            //Ajustando largura e altura dos itens automaticamnte
+            // principalmente do menu
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+
+            // Criando objeto Scene que é a cena principal
+            Scene mainScene = new Scene(scrollPane);
+            // Stage, palco da cena recebe a cena principal como argumento
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFx application"); // Título do palco
+            // Mostra o palco (com a cena)
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
