0451fa31b08c07a35912d188c087260040f19ad4
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-30 08:18:11 -0300

    InicializingActionWithParameter

--- src/main/java/com/dev/projectjavafxjdbc/MainViewController.java
+++ src/main/java/com/dev/projectjavafxjdbc/MainViewController.java
@@ -16,6 +16,7 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 public class MainViewController implements Initializable {
 
@@ -40,13 +41,19 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction() {
-        loadView2("DepartmentList.fxml");
+        loadView("DepartmentList.fxml", (DepartmentListController controller) -> {
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
+        });
     }
 
 
     @FXML
+    // como tem ação no load view para o método acima, esse daqui usamos
+    // uma expressão lambda passando nada como argumento porque esse n faz nada só
+    // imprimi na tela
     public void onMenuItemAboutAction() {
-        loadView("About.fxml");
+        loadView("About.fxml", x -> {});
     }
 
     @Override
@@ -56,7 +63,7 @@ public class MainViewController implements Initializable {
     }
 
     // Método para carregar a tela about.fxml
-    private synchronized void loadView(String absoluteName) {
+    private synchronized  <T> void  loadView(String absoluteName, Consumer<T> initializingAction) {
         // o synchronized é para que o método n seja interrompido
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -74,43 +81,19 @@ public class MainViewController implements Initializable {
             mainVBox.getChildren().add(mainMenu);
             mainVBox.getChildren().addAll(newVBox.getChildren()); // addAll é para adiconar coleção
 
+            // Comando para ativar a função da interface funcional Consumer
+            T controller = loader.getController();
+            initializingAction.accept(controller);
+            // essas 2 linhas acima executa função da expressão lambda
+            // do método que injeta o controller
+            // iss foi usado para criar um loadview único
+            // que carrega a tela e faz a injeção do controller
         } catch (IOException e) {
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
+
+
     }
+}
 
-        private synchronized void loadView2 (String absoluteName){
-            // o synchronized é para que o método n seja interrompido
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-                VBox newVBox = loader.load();
-
-                // Pegando referencia do VBOx da cena principal
-                Scene mainScene = HelloApplication.getMainScene();
-                // Get root pega o primeiro elemento da View Principal
-                // , ou seja o Scroll pane, depois o Conteúdo .content
-                VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-
-                // Trazendo para essa tela o menu bar e os labels abaixo dele
-                Node mainMenu = mainVBox.getChildren().get(0);
-                mainVBox.getChildren().clear();
-                mainVBox.getChildren().add(mainMenu);
-                mainVBox.getChildren().addAll(newVBox.getChildren()); // addAll é para adiconar coleção
-
-                // O loader carrega a view(tela), e partir do loader
-                // abaixo vamos usa-lo para acessar o controller
-                // pegando a referencia do controler, vamos
-                // injetar a depencia do service no controller
-                DepartmentListController controller = loader.getController();
-                // injetando depencencia do service no CONTROLLER
-                controller.setDepartmentService(new DepartmentService());
-                // chamando método de atualização do service na tableView (Tabela
-                controller.updateTableView();
-
-            } catch (IOException e) {
-                Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
-            }
-
-        }
-    }
 
